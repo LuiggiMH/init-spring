8f7a442c1c2616427e7747b3b2bf7941045c10f5
LuiggiMH <luiggimh@hotmail.com>
2022-08-19 07:58:29 -0500

    first commit

---
+++ src/main/java/ec/edu/itsqmet/initspring/InitSpringApplication.java
@@ -0,0 +1,13 @@
+package ec.edu.itsqmet.initspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InitSpringApplication.class, args);
+	}
+
+}
